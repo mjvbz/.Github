8bda696ec1ba57adb1c7126082c0faf78c5396d5
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-10-16 20:27:35 +0530

    added all files succesfully and updated to v1.0

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('button').onclick = () => {
+  if(document.querySelector('p').style.display = none) {
+    document.querySelector('p').style.display = block;
+    document.querySelector('h1').style.color = black;
+  } else {
+  document.querySelector('p').style.display = none;
+  document.querySelector('h1').style.color = red;
+  }
+};
